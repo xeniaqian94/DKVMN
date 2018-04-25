fix bug to have accurate f1 calculation

code/python3/load_data.py
@@ -1,5 +1,7 @@
 import numpy as np
 import math
+
+
 class DATA(object):
     def __init__(self, n_question, seqlen, separate_char, name="data"):
         # In the ASSISTments2009 dataset:
@@ -16,48 +18,54 @@ class DATA(object):
     ### 15
     ### 1,1,1,1,7,7,9,10,10,10,10,11,11,45,54
     ### 0,1,1,1,1,1,0,0,1,1,1,1,1,0,0
-    def load_data(self, path):
-        f_data = open(path , 'r')
+    def load_data(self, path, split_path=None):
+        f_data = open(path, 'r')
         q_data = []
         qa_data = []
+
+        if split_path is not None:
+            split_data = []
+            f_split_data = open(split_path, "r")
+            for lineID, line in enumerate(f_split_data):
+                split_data.append([int(value) for value in line.strip().split("\t")[1:]])
         for lineID, line in enumerate(f_data):
-            line = line.strip( )
+            line = line.strip()
             # lineID starts from 0
             if lineID % 3 == 1:
                 Q = line.split(self.separate_char)
-                if len( Q[len(Q)-1] ) == 0:
+                if len(Q[len(Q) - 1]) == 0:
                     Q = Q[:-1]
-                #print(len(Q))
+                # print(len(Q))
             elif lineID % 3 == 2:
                 A = line.split(self.separate_char)
-                if len( A[len(A)-1] ) == 0:
+                if len(A[len(A) - 1]) == 0:
                     A = A[:-1]
-                #print(len(A),A)
+                # print(len(A),A)
 
                 # start split the data
                 n_split = 1
-                #print('len(Q):',len(Q))
+                # print('len(Q):',len(Q))
                 if len(Q) > self.seqlen:
                     n_split = math.floor(len(Q) / self.seqlen)
                     if len(Q) % self.seqlen:
                         n_split = n_split + 1
-                #print('n_split:',n_split)
+                # print('n_split:',n_split)
                 for k in range(n_split):
                     question_sequence = []
                     answer_sequence = []
                     if k == n_split - 1:
-                        endINdex  = len(A)
+                        endINdex = len(A)
                     else:
-                        endINdex = (k+1) * self.seqlen
+                        endINdex = (k + 1) * self.seqlen
                     for i in range(k * self.seqlen, endINdex):
-                        if len(Q[i]) > 0 :
+                        if len(Q[i]) > 0:
                             # int(A[i]) is in {0,1}
                             Xindex = int(Q[i]) + int(A[i]) * self.n_question
                             question_sequence.append(int(Q[i]))
                             answer_sequence.append(Xindex)
                         else:
                             print(Q[i])
-                    #print('instance:-->', len(instance),instance)
+                    # print('instance:-->', len(instance),instance)
                     q_data.append(question_sequence)
                     qa_data.append(answer_sequence)
         f_data.close()
@@ -73,11 +81,14 @@ class DATA(object):
             dat = qa_data[j]
             qa_dataArray[j, :len(dat)] = dat
         # dataArray: [ array([[],[],..])] Shape: (3633, 200)
-        return q_dataArray, qa_dataArray
+        if split_path is None:
+            return q_dataArray, qa_dataArray
+        else:
+            return q_dataArray, qa_dataArray, split_data
 
     def generate_all_index_data(self, batch_size):
         n_question = self.n_question
-        batch = math.floor( n_question / self.seqlen )
+        batch = math.floor(n_question / self.seqlen)
         if self.n_question % self.seqlen:
             batch += 1
 
@@ -85,8 +96,7 @@ class DATA(object):
         zeroindex = np.arange(n_question, self.seqlen * batch)
         zeroindex = zeroindex.tolist()
         seq[zeroindex] = 0
-        q = seq.reshape((batch,self.seqlen))
+        q = seq.reshape((batch, self.seqlen))
         q_dataArray = np.zeros((batch_size, self.seqlen))
         q_dataArray[0:batch, :] = q
         return q_dataArray
-

code/python3/run.py
@@ -50,6 +50,7 @@ def compute_accuracy(all_target, all_pred):
 
 def train(net, params, q_data, qa_data, label):
     N = int(math.floor(len(q_data) / params.batch_size))
+    print("train_N "+str(N))
     q_data = q_data.T  # Shape: (200,3633)
     qa_data = qa_data.T  # Shape: (200,3633)
     # Shuffle the data
@@ -110,15 +111,17 @@ def train(net, params, q_data, qa_data, label):
     all_target = np.concatenate(target_list, axis=0)
 
     loss = binaryEntropy(all_target, all_pred)
-    print("all_target", all_target)
-    print("all_pred", all_pred)
+    # print("all_target", all_target, len(all_target))
+    # print("all_pred", all_pred, len(all_pred))
     auc = compute_auc(all_target, all_pred)
     accuracy = compute_accuracy(all_target, all_pred)
 
     return loss, accuracy, auc, compute_f1(all_target, all_pred)
 
 
-def test(net, params, q_data, qa_data, label):
+
+
+def test(net, params, q_data, qa_data, label, split_data=None):
     # dataArray: [ array([[],[],..])] Shape: (3633, 200)
     N = int(math.ceil(float(len(q_data)) / float(params.batch_size)))
     q_data = q_data.T  # Shape: (200,3633)
@@ -183,8 +186,38 @@ def test(net, params, q_data, qa_data, label):
     all_pred = np.concatenate(pred_list, axis=0)
     all_target = np.concatenate(target_list, axis=0)
 
+    print(all_pred,len(all_pred))
+    print(all_target,len(all_target))
+
+    if split_data is not None:
+        print(split_data)
+        all_pred,all_target=trim_valid_only(all_pred,all_target,split_data)
+
+    print("after triming to have valid only...")
+    print(all_pred, len(all_pred))
+    print(all_target, len(all_target))
+
     loss = binaryEntropy(all_target, all_pred)
     auc = compute_auc(all_target, all_pred)
     accuracy = compute_accuracy(all_target, all_pred)
 
     return loss, accuracy, auc, compute_f1(all_target, all_pred)
+
+
+
+def trim_valid_only(all_pred, all_target, split_data):
+    pred_list = []
+    target_list = []
+
+    start=0
+    end=0
+    for split_per_user in split_data:
+        start=end+split_per_user[0] # adds pure train length for this user
+        end=start+split_per_user[1] # adds pure valid length for this user
+        pred_list.append(all_pred[start:end])
+        target_list.append(all_target[start:end])
+
+    all_pred = np.concatenate(pred_list, axis=0)
+    all_target = np.concatenate(target_list, axis=0)
+
+    return all_pred,all_target